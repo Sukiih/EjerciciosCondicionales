4b6d25c784a521f8c692c9b9974ea6930743517d
Sukiih <vale.sgonzalez2011@hotmail.com>
2025-04-29 22:18:00 -0400

    quinto ejercicio: descuento compra

--- src/DescuentoCompra.java
+++ src/DescuentoCompra.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class DescuentoCompra {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-
         System.out.println("Ingrese el valor de la compra:");
         double valorCompra = teclado.nextDouble();
 
