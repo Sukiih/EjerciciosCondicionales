996b8b25bfdc202e31b2b6e3fefd1424941873c6
Sukiih <vale.sgonzalez2011@hotmail.com>
2025-05-01 14:14:58 -0400

    quinto ejercicio: descuento compra (cambio en sout)

--- src/DescuentoCompra.java
+++ src/DescuentoCompra.java
@@ -9,10 +9,11 @@ public class DescuentoCompra {
         if (valorCompra >= 100.00) {
             double descuento = valorCompra * 0.10;
             double totalConDescuento = valorCompra - descuento;
-            System.out.println("¡Descuento aplicado del 10%!");
-            System.out.println("Total a pagar con descuento: $" + totalConDescuento);
+            System.out.println("Descuento del 10% aplicado.");
+            System.out.println("Nuevo valor: $" + totalConDescuento);
         } else {
-            System.out.println("No se aplica descuento. Total a pagar: $" + valorCompra);
+            System.out.println("Descuento no aplicado.");
+            System.out.println("Valor total: $" + valorCompra);
         }
 
         teclado.close();
